4f4d390238aa930413105b904ac84f27775f0ac6
JENNA PARKER <github-actions[bot]@users.noreply.github.com>
2022-11-04 17:51:53 -0400

    inserts the emailjs form

---
+++ src/views/Contact.vue
@@ -0,0 +1,35 @@
+<template>
+    <form ref="form" @submit.prevent="sendEmail">
+      <label>Name</label>
+      <input type="text" name="user_name">
+      <label>Email</label>
+      <input type="email" name="user_email">
+      <label>Message</label>
+      <textarea name="message"></textarea>
+      <input type="submit" value="Send">
+    </form>
+  </template>
+
+
+<style>
+form {
+    color: white;
+}
+</style>
+  
+  <script>
+  import emailjs from '@emailjs/browser';
+  
+  export default {
+    methods: {
+      sendEmail() {
+        emailjs.sendForm('service_bvk1rmn', 'template_xly6jps', this.$refs.form, 'TbRiNitiwUZuV34HP')
+          .then((result) => {
+              console.log('SUCCESS!', result.text);
+          }, (error) => {
+              console.log('FAILED...', error.text);
+          });
+      }
+    }
+  }
+  </script>
