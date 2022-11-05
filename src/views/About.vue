8766ba6e25ffeb83e0b59a4b2c9d1e32247d855d
JENNA PARKER <github-actions[bot]@users.noreply.github.com>
2022-11-05 13:34:38 -0400

    inserts a picture of me on the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,44 @@
 
 <template>
+<div class="all">
+<img src="https://pbs.twimg.com/profile_images/1588947438320193536/QT_O7sk__400x400.jpg"/>
+
+
         <div class="about">
             
             <p class="chunk">
-                Hi! My name is Jenna, and I graduated from the <a
+                <span class="hi">Hi! </span> My name is Jenna, and I graduated from the <a
                     href="https://generalassemb.ly/education/software-engineering-immersive"><span
                         class="linked">General Assembly Software
-                        Engineering Immersive</span></a> on
-                10-31-22, in order to fulfill my desired persuit to further understand the tech space and contribute to
+                        Engineering Immersive bootcamp</span></a> on
+                10-31-22, in order to fulfill my desired pursuit to further understand the tech space and contribute to
                 it in a meaningful way. </p>
 
             <p class="chunk"> I am a Georgia State University 2021 alumn with a Bachelor's Degree in Journalism and a
-                great passion for storytelling which I strive to bring software engineering through clear, intentionally
+                great passion for storytelling, which I strive to bring to software engineering through
                 <span>optomized user stories</span> and <span>captivating design.</span>
             </p>
             <p class="chunk">
                 I have 5+ years of customer service experience as a brunch cook, a bartender at a retirement community,
-                a sales associate at Ponce City Market and an event coordinator at a community arts center. </p>
+                a sales associate at Ponce City Market and an event coordinator at a community arts center. I am a food-lover, a Mom to my black cat Rory, a twin, an Aunt and a life-long student. </p>
         </div>
+    </div>
 </template>
+
+<style>
+.hi {
+    color: #0677a1;
+    font-size: 50px;
+}
+
+img {
+    width: 600px;
+    height: 600px;
+    margin-left: 10vh;
+    margin-top: 7vh;
+}
+
+.all {
+    display: flex;
+}
+</style>
