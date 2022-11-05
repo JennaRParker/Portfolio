8bd8777ef8ffd9397cdb2a7d4f0bbb07c58a38db
JENNA PARKER <github-actions[bot]@users.noreply.github.com>
2022-11-05 12:30:57 -0400

    styles the contact form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,19 +1,55 @@
 <template>
-     <h1>Send me a message, I'd love to hear from you!</h1>
+     <h1 id="contact">Send me a message. I'd love to hear from you!</h1>
     <form ref="form" @submit.prevent="sendEmail">
-      <input placeholder="Your Name" type="text" name="user_name"> <br />
+      <input placeholder="Your Name" type="text" name="user_name"> 
       <input placeholder="Your Email" type="email" name="user_email"> <br />
       <textarea placeholder="Your Message" name="message"></textarea> <br />
-      <input type="submit" value="Send">
+      <input id="send" type="submit" value="Send">
     </form>
   </template>
 
 
 <style>
+#contact {
+    text-align: center;
+    margin-top: 10vh;
+    font-size: 5vh
+}
+
 form {
     color: white;
     font-family: 'Righteous', cursive;
     text-align: center;
+}
+
+input {
+    width: 50vh;
+    height: 10vh;
+    text-align: center;
+    font-family: 'Righteous', cursive;
+    font-size: 25px;
+    box-shadow: white 1px 0 30px;
+    
+}
+
+textarea {
+    width: 100vh;
+    height: 30vh;
+    border-width: 5px;
+    border-color: white;
+    color: white;
+    background-color: black;
+    font-size: larger;
+    box-shadow: white 1px 0 30px;
+}
+
+#send {
+    width: 102vh;
+    border-width: 10px;
+    border-color: #0677a1;
+    background-color: #0677a1;
+    color: white;
+    font-size: 40px;
 }
 </style>
   
